8d687f54096c4e1cf61d9ddf4c4d12d62618711b
Vic <ab889721@gmail.com>
2018-06-26 00:13:28 +0800

    update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,21 +1,36 @@
 <template>
 <div id="TodoList">
-  <!-- 可使用.trim去除空白 -->
-  <input type="text" v-bind:placeholder="placeholder" v-model.trim="todoMessage">
 
-  <!-- v-on:click === @click -->
-  <button v-on:click="addTodo">Add Todo</button>
+  <div class= "top-area">
+    <!-- 可使用.trim去除空白 -->
+    <input type="text" v-bind:placeholder="placeholder" v-model.trim="todoMessage">
+
+    <!-- v-on:click === @click -->
+    <button v-on:click="addTodo">Add Todo</button>
+    <!-- v-if 不會先渲染 ,v-show 用 display:none -->
+
+  </div>
   <div id="content">
-    <ul class="todolist">
+        <div v-if="isMaxLimit">最多只能有{{maxLimit}}筆待辦事項！</div>
+    共有: {{todoList.length}} 筆事項，有 {{checkedTodos.length}} 筆完成
+    <ol class="todolist">
       <!-- v-bind:key === :key  -->
-      <li v-bind:class="todolist-item" v-for="(todo,index) in todoList" v-bind:key="index">
+      <li v-bind:class="[todolist-item,todo.checked?'todolist-item--notshow':'']" v-for="(todo,index) in todoList" v-bind:key="index">
         <input v-bind:id="`todo-${index}`" type="checkbox" v-model="todo.checked">
         <label v-bind:class="{'todolist-item-checked':todo.checked}"
-               v-bind:for="`todo-${index}`" >{{todo.name}}</label>
+               v-bind:for="`todo-${index}`">{{todo.name}}</label>
         <button @click="removeTodo(index)">X</button>
       </li>
-    </ul>
-    {{todoList}}
+    </ol>
+    <!-- {{todoList}} -->
+
+    <div class="checkedList">
+      已完成清單：
+      <li v-bind:class="todolist-item" v-for="(checkedTodo,index) in checkedTodos" v-bind:key="index">
+        {{checkedTodo.name}}
+        <button @click="removeTodo(index)">X</button>
+      </li>
+    </div>
   </div>
 
 </div>
@@ -28,12 +43,23 @@ export default {
     return {
       placeholder: 'Please add something',
       todoMessage: '',
-      todoList: [{ name: 'Learn Vue.js', checked: false }]
+      todoList: [{ name: 'Learn Vue.js', checked: false }],
+      maxLimit: 3
+    }
+  },
+  computed: {
+    checkedTodos() {
+      return this.todoList.filter(todo => todo.checked)
+    },
+    isMaxLimit() {
+      return this.todoList.length === this.maxLimit
     }
   },
   methods: {
     addTodo() {
-      this.todoList.push({ name: this.todoMessage })
+      if (!this.isMaxLimit && this.todoMessage) {
+        this.todoList.push({ name: this.todoMessage })
+      }
       this.todoMessage = ''
     },
     removeTodo(index) {
@@ -46,10 +72,32 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 #TodoList {
+  * {
+    font-size: 1.2em;
+  }
+  .top {
+    display: flex;
+
+    &-area {
+      > * {
+        flex: 1;
+        flex-basis: 30px;
+      }
+      display: flex;
+      // flex-direction: column;
+    }
+  }
+
   .todolist {
     &-item {
+      display: flex;
+      align-items: center;
+      text-align: center;
       &-checked {
         text-decoration: line-through;
+      }
+      &--notshow {
+        display: none;
       }
     }
   }
