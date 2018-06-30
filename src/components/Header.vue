70d636ad84bdd8969c2b171865a6411d9ae48c24
Vic <ab889721@gmail.com>
2018-06-30 22:04:56 +0800

    done basic todolist practice

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+      <h1>{{titleName}}</h1>
+      <div>更換標題(子元件Header 傳向父元件 TodoList)<input type="text" v-model="msg" @change="inputChange" @keyup="inputChange"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      header: 'Vue.js TodoList',
+      msg: ''
+    }
+  },
+  props: {
+    titleName: {
+      type: String,
+      default: '預設標題'
+    }
+  },
+  methods: {
+    inputChange() {
+      this.$emit('headerChange', this.msg)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="TodoList">
-
+  <Header :titleName="titleName" v-on:headerChange="getNewHeader"></Header>
   <div class= "top-area">
     <!-- 可使用.trim去除空白 -->
     <input type="text" v-bind:placeholder="placeholder" v-model.trim="todoMessage">
@@ -8,22 +8,25 @@
     <!-- v-on:click === @click -->
     <button v-on:click="addTodo">Add Todo</button>
     <!-- v-if 不會先渲染 ,v-show 用 display:none -->
+    <div>共有: {{todoList.length}} 筆事項，有 {{checkedTodos.length}} 筆完成</div>
 
   </div>
   <div id="content">
         <div v-if="isMaxLimit">最多只能有{{maxLimit}}筆待辦事項！</div>
-    共有: {{todoList.length}} 筆事項，有 {{checkedTodos.length}} 筆完成
+    <div>待辦清單</div>
+
     <ol class="todolist">
+
       <!-- v-bind:key === :key  -->
       <li v-bind:class="[todolist-item,todo.checked?'todolist-item--notshow':'']" v-for="(todo,index) in todoList" v-bind:key="index">
         <input v-bind:id="`todo-${index}`" type="checkbox" v-model="todo.checked">
+        <span v-show="todo.checked">✔️</span>
         <label v-bind:class="{'todolist-item-checked':todo.checked}"
                v-bind:for="`todo-${index}`">{{todo.name}}</label>
         <button @click="removeTodo(index)">X</button>
       </li>
     </ol>
     <!-- {{todoList}} -->
-
     <div class="checkedList">
       已完成清單：
       <li v-bind:class="todolist-item" v-for="(checkedTodo,index) in checkedTodos" v-bind:key="index">
@@ -37,10 +40,16 @@
 </template>
 
 <script>
+import Header from '@/components/Header'
+
 export default {
   name: 'TodoList',
+  components: {
+    Header
+  },
   data() {
     return {
+      titleName: `Vue.js TodoList 練習`,
       placeholder: 'Please add something',
       todoMessage: '',
       todoList: [{ name: 'Learn Vue.js', checked: false }],
@@ -64,6 +73,9 @@ export default {
     },
     removeTodo(index) {
       this.todoList.splice(index, 1)
+    },
+    getNewHeader(msg) {
+      this.titleName = msg
     }
   }
 }
@@ -72,27 +84,48 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 #TodoList {
+  border: 1px solid black;
+
+  width: 600px;
+  height: auto;
+  margin: 0 auto;
   * {
     font-size: 1.2em;
   }
   .top {
-    display: flex;
-
     &-area {
+      border-bottom: 1px solid black;
+
       > * {
         flex: 1;
         flex-basis: 30px;
       }
-      display: flex;
       // flex-direction: column;
     }
   }
 
   .todolist {
-    &-item {
+    padding: 5px;
+    margin: 0;
+    border-bottom: 1px solid black;
+    > li {
+      text-align: left;
       display: flex;
-      align-items: center;
-      text-align: center;
+      width: 100%;
+      > input {
+        width: 30px;
+        height: 30px;
+      }
+      > button {
+        width: 30px;
+        flex: 1;
+      }
+      > label {
+        flex: 8;
+      }
+    }
+
+    &-item {
       &-checked {
         text-decoration: line-through;
       }
